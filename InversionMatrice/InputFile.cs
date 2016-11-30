6e39e4ebb56dfe487f476b35cf7ec991ce46acf0
Kevin Marra <kevin.marra94@hotmail.fr>
2016-11-30 22:05:36 +0100

    Ajout de la lecture d'une matrice à partir d'un fichier.

---
+++ InversionMatrice/InputFile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversionMatrice
+{
+    class InputFile
+    {
+        private string[] lines;
+        private string path;
+
+        public InputFile()
+        {
+            path = "input.txt";
+            lines = null;
+        }
+
+        public InputFile(String path)
+        {
+            this.path = path;
+            lines = null;
+        }
+
+        public Matrice ReadFile()
+        {
+            try
+            {
+                lines = System.IO.File.ReadAllLines(path);
+                return LoadMatrice();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }            
+        }
+
+        private Matrice LoadMatrice()
+        {
+            Matrice m = null;
+            int ln, col;
+            String[] temp;
+
+            temp = lines[0].Split(' ');
+
+            ln = lines.Length;
+            col = temp.Length;
+            m = new Matrice(ln, col);
+
+            for (int i = 0; i < ln; i++)
+            {
+                temp = lines[i].Split(' ');
+
+                for (int j = 0; j < col ; j++)
+                {
+                    m[i, j] = double.Parse(temp[j]);
+                }
+            }
+
+            return m;
+        }
+    }
+}
